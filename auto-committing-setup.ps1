db1cc48d9eb05a0222c8e939d9cc66921c0b1606
Kristen Thoma <kristenthoma@gmail.com>
2021-09-21 12:05:35 -0400

    Commit

---
+++ auto-committing-setup.ps1
@@ -0,0 +1,6 @@
+$GitHubUsername = 'Kristen-Thoma'
+$PracticeRepoDir = 'C:/Users/krist/powershell-scripting'
+$CommitMessage = 'auto committed from auto-committing-setup.ps1!'
+
+git clone "https://github.com/Kristen-Thoma/powershell-practice" "$PracticeRepoDir"
+
