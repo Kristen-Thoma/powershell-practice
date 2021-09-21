0eb1a2f6cad0fb4a9630e177b454e25698c53886
Kristen Thoma <kristenthoma@gmail.com>
2021-09-21 13:01:51 -0400

    auto committed from auto-committing-setup.ps1!

--- auto-committing-setup.ps1
+++ auto-committing-setup.ps1
@@ -4,3 +4,13 @@ $CommitMessage = 'auto committed from auto-committing-setup.ps1!'
 
 git clone "https://github.com/Kristen-Thoma/powershell-practice" "$PracticeRepoDir"
 
+Copy-Item C:\Users\krist\auto-committing-setup.ps1 C:\Users\krist\powershell-scripting\auto-committing-setup.ps1
+
+Set-Location "$PracticeRepoDir"
+
+git add .
+
+git commit -m "$CommitMessage"
+
+git push
+
